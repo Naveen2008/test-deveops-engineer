0b7e9c12ba2181afa46388f47ad79e28f73a54ac
Naveen Naidu <naidu@MacBookPro.modem>
2025-01-15 16:10:12 +1100

    Refactor metrics code

--- node-app/server.js
+++ node-app/server.js
@@ -1,58 +1,34 @@
 'use strict'
 
 const express = require('express');
-const promClient = require('prom-client');
+const client = require('prom-client');
 
-// Create a Registry to collect metrics
-const register = new promClient.Registry();
-
-// Default metrics (e.g., CPU, memory, etc.)
-promClient.collectDefaultMetrics({ register });
-
-const PORT = 8080;
+const app = express();
+const PORT = process.env.PORT || 9000;
 const HOST = '0.0.0.0';
 const OS = require('os');
 const ENV = 'PROD';
 
-const app = express();
+
+// Default metrics (e.g., CPU, memory, etc.)
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register })
+
+
 app.get('/', (req, res) => {
     res.statusCode = 200;
     const msg = 'Hello from from Playa3ull!!!!';
     res.send(getPage(msg));
 });
 
-app.get('/test', (req, res) => {
-    res.statusCode = 200;
-    const msg = 'Hello from /test Node!';
-    res.send(getPage(msg));
-});
+app.get("/metrics", async (req, res) => {
+    res.setHeader('Content-Type', client.register.contentType);
+    const metrics = await client.register.metrics();
+    res.send(metrics)
+})
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
-});
-
-
-// Custom metrics
-const httpRequestDuration = new promClient.Histogram({
-    name: 'http_request_duration_seconds',
-    help: 'Duration of HTTP requests in seconds',
-    labelNames: ['method', 'route', 'status'],
-    buckets: [0.1, 0.3, 0.5, 1, 3, 5],
-});
-register.registerMetric(httpRequestDuration);
-
-app.use((req, res, next) => {
-    const end = httpRequestDuration.startTimer();
-    res.on('finish', () => {
-        end({ method: req.method, route: req.route?.path || req.url, status: res.statusCode });
-    });
-    next();
-});
-
-// Add a /metrics endpoint
-app.get('/metrics', async (req, res) => {
-    res.set('Content-Type', register.contentType);
-    res.end(await register.metrics());
 });
 
 app.listen(PORT, HOST);
